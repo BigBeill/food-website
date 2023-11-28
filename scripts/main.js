37dfea1aae3a5ba9a69cf2b92070b6d19d875f9f
Mackenzie Neill <mackenzie.neill.730@gmail.com>
2023-11-28 02:44:52 -0500

    navBar-01

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+document.getElementById('navBar').innerHTML = 
+` <img class="logo" src="./images/BigBeill-logo_black.png" alt="Beill Greenhouse Logo">
+
+<h3>Find Recipes</h3>
+<a href="index.html">Public</a>
+<a href="index.html">Friends</a>
+<a href="index.html">Search</a>
+
+<h3>Your Recipes</h3>
+<a href="index.html">Owned</a>
+<a href="index.html">Saved</a>
+
+<h3>Account</h3>
+<a href="login.html">Login</a>
+<a href="register.html">Create Account</a> `;
