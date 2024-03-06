4c56376b4f3a26b29fe9e98e7bf36c7c11d02622
Mackenzie Neill <mackenzie.neill.730@gmail.com>
2024-03-06 18:23:39 -0500

    more fixes
    whatever i tried to do with the merge crap really fucked things up. this should be the final fix

--- routes/index.route.js
+++ routes/index.route.js
@@ -4,10 +4,21 @@ const mongoConnection = require('../config/connectMongo')
 const processQuery = require('../models/mongoQuery')
 const recipes = require('../schemas/recipe')
 
+
+
+// ------------ index get requests ------------
 router.get("/", async (req, res) => {
     var result
     result = await recipes.find()
     res.render("index", {recipes: result})
 })
+
+router.get("/search", async (req,res) => {
+    var result
+    result = await recipes.find()
+    res.render("index", {recipes: result})
+})
+
+
 
 module.exports = router
