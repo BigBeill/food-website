f1ad70b64b8597d6e64bf79c19f1f8f13c1d2a83
Mackenzie Neill <mackenzie.neill.730@gmail.com>
2024-03-06 17:33:59 -0500

    fixed search bar
    turned the search bar into a partial and have it imported directly by the main view

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const router = express.Router()
-const mongoConnection = require('../config/connectMongo')
-const processQuery = require('../models/mongoQuery')
 const recipes = require('../schemas/recipe')
 
+
+
+// ------------ index get routes ------------
 router.get("/", async (req, res) => {
     var result
     result = await recipes.find()
     res.render("index", {recipes: result})
 })
+
+router.get("/search", async (req, res) => {
+    var result
+    res.render("index", {recipes: result})
+})
+
+
+
+// ------------ index post routes ------------
+
+
 
 module.exports = router
