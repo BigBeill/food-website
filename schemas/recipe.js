5e22e1eefd085d7e3765b20b2802694f2a6c14fe
Connor Pink <77735965+connorpink@users.noreply.github.com>
2024-03-08 12:18:29 -0500

    added rest of image(forseeably emoji) code

--- schemas/recipe.js
+++ schemas/recipe.js
@@ -4,7 +4,7 @@ const recipeSchema = new mongoose.Schema({
     owner: mongoose.SchemaTypes.ObjectId,
     title: String,
     description: String,
-    image: {type: String, enum: ['129374', '127789', '127828']}
+    image: {type: String, enum: ['🧀', '🥞', '🍗', '🍔']}
 })
 
 module.exports = mongoose.model("recipe", recipeSchema)
