b18a65ff74f8ccedefa58913962f727d741fbb3b
Mackenzie Neill <mackenzie.neill.730@gmail.com>
2024-04-26 23:55:11 -0400

    editReipe - 04
    not sure why this didnt get included with the last commit

--- client/src/pages/editRecipe.jsx
+++ client/src/pages/editRecipe.jsx
@@ -3,6 +3,18 @@ import React, { useRef, useState, useEffect } from 'react'
 function createRecipe () {
   const [title, setTitle] = useState("")
   const [description, setDescription] = useState("")
+  const [instructionList, setInstructionList] = useState([])
+  const [newInstruction, setNewInstruction] = useState("")
+
+  function addIngredient() {
+
+  }
+
+  function addInstruction() {
+    setInstructionList((list) => {
+      return [...list, newInstruction]
+    })
+  }
 
   return(
     <>
@@ -16,19 +28,61 @@ function createRecipe () {
             <input 
             type="text" 
             id="title"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
             placeholder="Name your recipe" />
           </div>
           <div className='textInput'>
-          <label htmlFor="description">Recipe Description</label>
+            <label htmlFor="description">Recipe Description</label>
             <textarea
             type="text" 
             id="description"
             rows="12"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
             placeholder="Describe your recipe" />
           </div>
         </div>
         <div>
-
+          <h2>image</h2>
+        </div>
+      </div>
+      <div className='splitSpace'>
+        <div>
+          <h2>Ingredients</h2>
+          <button
+          name="addIngredient"
+          id="addIngredientButton"
+          className="addItemButton"
+          onClick={addIngredient}
+          >+</button>
+        </div>
+        <div>
+          <h2>Instructions</h2>
+          <div>
+            {instructionList.map((instruction, index) => (
+              <>
+              <h4>Step {index + 1}</h4>
+              <p>{instruction}</p>
+              </>
+            ))}
+          </div>
+          <div className="textInput withButton">
+            <label htmlFor="newInstruction"><h3>new step</h3></label>
+            <textarea
+            type="text" 
+            id="newInstruction"
+            rows="3"
+            value={newInstruction}
+            onChange={(event) => setNewInstruction(event.target.value)}
+            placeholder="Add instruction" />
+            <button
+            name="addInstruction"
+            id="addInstructionButton"
+            className="addItemButton"
+            onClick={addInstruction}
+            >+</button>
+          </div>
         </div>
       </div>
     </div>
