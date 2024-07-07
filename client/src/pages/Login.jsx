2c11518b435f4ce4ce6915dcf8e5308cb11bc0d6
Connor Pink <77735965+connorpink@users.noreply.github.com>
2024-07-07 19:11:20 -0400

    modified esLint for errors. Also added enter key function for login page.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable react/no-unknown-property */
 import React, { useRef, useState, useEffect } from 'react'
 
-function Login(){
+function Login() {
   const errorRef = useRef()
 
   const [username, setUsername] = useState("")
@@ -14,10 +15,13 @@ function Login(){
   useEffect(() => {
     setErrorMessage("");
   }, [username, password])
- 
-  function attemptLogin(){
-    if (!username){setErrorMessage("no username given")}
-    else if (!password){setErrorMessage("no password given")}
+
+
+  function attemptLogin() {
+
+
+    if (!username) { setErrorMessage("no username given") }
+    else if (!password) { setErrorMessage("no password given") }
     else {
       const postRequest = {
         method: 'POST',
@@ -29,68 +33,67 @@ function Login(){
       }
 
       fetch("server/user/login", postRequest)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        if (data.message == "badUser"){setErrorMessage("Username not found")}
-        if (data.message == "badPass"){setErrorMessage("Password incorrect")}
-        if (data.message == "success"){location.assign('/')}
-      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          if (data.message == "badUser") { setErrorMessage("Username not found") }
+          if (data.message == "badPass") { setErrorMessage("Password incorrect") }
+          if (data.message == "success") { location.assign('/') }
+        })
     }
   }
 
-  return(
+  return (
     <>
-    <div className="loginForm" id="loginForm">
-
-      <h1>Login</h1>
-      
-      <div className="textInput">
-        <input 
-        type="text" 
-        name="username" 
-        id="username" 
-        placeholder=' '
-        value={username}
-        onChange={(event) => setUsername(event.target.value)}
-        />
-        <label htmlFor="username">Username</label>
-      </div>
-
-      <div className="textInput">
-        <input 
-        type="password" 
-        name="password" 
-        id="password" 
-        placeholder=' '
-        value={password}
-        onChange={(event) => setPassword(event.target.value)}
-        />
-        <label htmlFor="password">Password</label>
-      </div>
-
-      <div className="splitSpace">
-      <div className="checkboxInput">
-            <input type="checkbox"
-            name="remember me" 
-            id="remember" 
-            value="1"/>
-            <label htmlFor="remember">Remember Me</label>
+      <div className="loginForm" id="loginForm">
+        <h1>Login</h1>
+        <div className="textInput">
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder=' '
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            onKeyDown={(event) => { if (event.key === 'Enter') { attemptLogin() } }}
+          />
+          <label htmlFor="username">Username</label>
         </div>
+
+        <div className="textInput">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder=' '
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            onKeyDown={(event) => { if (event.key === 'Enter') { attemptLogin() } }}
+          />
+          <label htmlFor="password">Password</label>
+        </div>
+
+        <div className="splitSpace">
+          <div className="checkboxInput">
+            <input type="checkbox"
+              name="remember me"
+              id="remember"
+              value="1" />
+            <label htmlFor="remember">Remember Me</label>
+          </div>
+        </div>
+
+        <button
+          name="Submit"
+          type="submit"
+          id="submitButton"
+          onClick={attemptLogin}
+        > Login </button>
+        <p ref={errorRef} className={errorMessage ? "error" : "hidden"} area-live="assertive">{errorMessage}</p>
+        <p>Don&apos;t have an account?</p>
+        <a href='/register'>create account</a>
+
       </div>
-
-      <button 
-      name="Submit" 
-      id="submitButton"
-      onClick={attemptLogin}
-      > Login </button>
-
-      <p ref={errorRef} className={errorMessage ? "error" : "hidden"} area-live="assertive">{errorMessage}</p>
-
-      <p>Don&apos;t have an account?</p>
-      <a href='/register'>create account</a>
-      
-    </div>
     </>
   )
 }
