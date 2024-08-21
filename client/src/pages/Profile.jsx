a3d0dc4082005cf9ad2f53abd2621b7d294b3b3a
MrPink925 <cpinkster3@gmail.com>
2024-08-21 01:03:26 -0400

    axios and profile page changes
    updated axios to remove vulnerability
    changed profile page structure
    updated profile page to use axios for logout

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,22 +1,54 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useOutletContext } from 'react-router-dom';
 import UserPin from '../components/UserPin'
+import axios from 'axios';
+
 
 function Profile() {
   const navigate = useNavigate();
   const { userData } = useOutletContext();
-  
+
+  // handle logout function
+  const handleLogout = () => {
+    const postRequest = {
+      method: 'POST',
+      url: 'server/user/logout',
+
+    }
+    axios(postRequest)
+      .then(response => {
+        console.log(response);
+        if (response.data.message == "success") { location.assign('/') }
+      })
+      .catch(error => {
+        console.error("unable to logout", error)
+      })
+  };
+
   //some placeholder text while page is waiting for user data to be collected
-  if (!userData.collected) return(<p>collecting user data</p>);
+  if (!userData.collected) return (<p>collecting user data</p>);
 
   //make sure user is signed in before trying to render there profile
   if (!userData._id) navigate('/login');
 
   return (
     <div className='profile splitSpace'>
-      <UserPin userData={userData} />
+      <div className="userPin">
+        <div className="splitSpace">
+          <UserPin userData={userData} />
+          <div className="profileButtons">
+            <button>edit account -&gt; </button>
+            <button
+              onClick={handleLogout}
+
+            >logout -&gt; </button>
+          </div>
+        </div>
+
+      </div >
     </div >
   )
 }
+
 
 export default Profile
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -3,118 +3,134 @@ const refreshTokens = require("../models/refreshToken");
 const validPassword = require("../library/passwordUtils").validPassword;
 const createToken = require("../config/jsonWebToken");
 const { verify } = require("jsonwebtoken");
-require('dotenv').config();
+require("dotenv").config();
 
 // the max age of all cookies created by this controller
 const cookieAge = 1000 * 60 * 60 * 24 * 30; // 30 days in milliseconds
 
-
 exports.register = async (req, res) => {
-  const {username, email, password} = req.body;
+  const { username, email, password } = req.body;
 
   // check for missing data
-  if (!username) { return res.status(400).json({ error: 'no username provided' }); }
-  if (!email) { return res.status(400).json({ error: 'no email provided' }); }
-  if (!password) { return res.status(400).json({ error: 'no password provided' }); }
+  if (!username) {
+    return res.status(400).json({ error: "no username provided" });
+  }
+  if (!email) {
+    return res.status(400).json({ error: "no email provided" });
+  }
+  if (!password) {
+    return res.status(400).json({ error: "no password provided" });
+  }
 
   //make sure username or email isn't already taken
   const searchUsername = await users.findOne({ username });
-  if (searchUsername) { return res.status(400).json({ error: 'username already taken' }); }
+  if (searchUsername) {
+    return res.status(400).json({ error: "username already taken" });
+  }
   const searchEmail = await users.findOne({ email });
-  if (searchEmail) { return res.status(400).json({ error: 'email already taken' }) }
+  if (searchEmail) {
+    return res.status(400).json({ error: "email already taken" });
+  }
 
   const hashedPassword = genPassword(password);
 
   // save user to database
   try {
-    const newUser = { username, email, hash: hashedPassword.hash, salt: hashedPassword.salt }
-    await new users(newUser).save()
-    console.log('new user created:', newUser)
-  } 
-  catch(error){
-    console.log('failed to create new user:', newUser); 
+    const newUser = {
+      username,
+      email,
+      hash: hashedPassword.hash,
+      salt: hashedPassword.salt,
+    };
+    await new users(newUser).save();
+    console.log("new user created:", newUser);
+  } catch (error) {
+    console.log("failed to create new user:", newUser);
     console.log(error);
-    return res.status(500).json({ error: 'server failed to create new user' })
+    return res.status(500).json({ error: "server failed to create new user" });
   }
 
   // send cookies to client
-  try { 
+  try {
     const tokens = createToken(newUser);
     await new refreshTokens({
       user: user._id,
-      token: tokens.refreshToken
-    }).save(); 
-    res.cookie("accessToken", tokens.accessToken, { maxAge: cookieAge });
-    res.cookie("refreshToken", tokens.refreshToken, { maxAge: cookieAge }); 
-    return res.status(200).json({ message: 'register successful'})
-  }
-  catch (error){
-    console.error("error saving refresh token for user:", user);
-    console.error(error);
-    return res.status(500).json({ error: "server issue while saving refresh token" });
-  }
-}
-
-
-
-exports.login = async (req, res) => {
-  const {username, password} = req.body;
-
-  // make sure data provided is valid
-  const user = await users.findOne({ username }, { _id:1, username:1, email:1, hash:1, salt:1 });
-  if (!user) { return res.status(400).json({ error: 'username not found' }); }
-  if (!validPassword(password, user.hash, user.salt)) { return res.status(400).json({ error: 'incorrect password' }) }
-
-  // return cookies to client
-  try { 
-    const tokens = createToken(user);
-    await new refreshTokens({
-      user: user._id,
-      token: tokens.refreshToken
+      token: tokens.refreshToken,
     }).save();
     res.cookie("accessToken", tokens.accessToken, { maxAge: cookieAge });
     res.cookie("refreshToken", tokens.refreshToken, { maxAge: cookieAge });
-    return res.status(200).json({ message: "user Signed in"}) 
-  }
-  catch (error){
+    return res.status(200).json({ message: "register successful" });
+  } catch (error) {
     console.error("error saving refresh token for user:", user);
     console.error(error);
-    return res.status(500).json({ error: "server issue while saving refresh token" });
+    return res
+      .status(500)
+      .json({ error: "server issue while saving refresh token" });
   }
-}
+};
 
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
 
+  // make sure data provided is valid
+  const user = await users.findOne(
+    { username },
+    { _id: 1, username: 1, email: 1, hash: 1, salt: 1 }
+  );
+  if (!user) {
+    return res.status(400).json({ error: "username not found" });
+  }
+  if (!validPassword(password, user.hash, user.salt)) {
+    return res.status(400).json({ error: "incorrect password" });
+  }
+
+  // return cookies to client
+  try {
+    const tokens = createToken(user);
+    await new refreshTokens({
+      user: user._id,
+      token: tokens.refreshToken,
+    }).save();
+    res.cookie("accessToken", tokens.accessToken, { maxAge: cookieAge });
+    res.cookie("refreshToken", tokens.refreshToken, { maxAge: cookieAge });
+    return res.status(200).json({ message: "user Signed in" });
+  } catch (error) {
+    console.error("error saving refresh token for user:", user);
+    console.error(error);
+    return res
+      .status(500)
+      .json({ error: "server issue while saving refresh token" });
+  }
+};
 
 exports.refresh = async (req, res) => {
   const refreshToken = req.cookies["refreshToken"];
 
   // make sure refresh token exists in database
-  const databaseToken = await refreshTokens.findOne({ token: refreshToken});
-  if (!databaseToken) return res.status(403).json({ error: "invalid refresh token"})
+  const databaseToken = await refreshTokens.findOne({ token: refreshToken });
+  if (!databaseToken)
+    return res.status(403).json({ error: "invalid refresh token" });
 
   // validate the refresh token and send a new access token
   try {
     const validToken = verify(refreshToken, process.env.SESSION_SECRET);
     if (validToken && validToken._id == databaseToken.user) {
-      const tokens = createToken({ _id: validToken._id, username: validToken.username });
+      const tokens = createToken({
+        _id: validToken._id,
+        username: validToken.username,
+      });
       res.cookie("accessToken", tokens.accessToken, { maxAge: cookieAge });
-      return res.status(200).json({ message: "new access token sent" })
+      return res.status(200).json({ message: "new access token sent" });
     }
   } catch (error) {
-    console.error('error creating new access token:', refreshToken);
+    console.error("error creating new access token:", refreshToken);
     console.error(error);
-    return res.status(403).json({ error: "could not create new access token"})
+    return res.status(403).json({ error: "could not create new access token" });
   }
-
-}
-
-
-
-
-
+};
 
 exports.logout = async (req, res) => {
-  res.clearCookie('accessToken');
-  res.clearCookie('refreshToken');
-  res.status(200).json({ message: "cookies removed" })
-}
+  res.clearCookie("accessToken");
+  res.clearCookie("refreshToken");
+  res.status(200).json({ message: "success" });
+};
