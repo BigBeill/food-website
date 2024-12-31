27d9a1a833faf7a9855e08f076acbb47305f59a1
Mackenzie Neill <128575514+BigBeill@users.noreply.github.com>
2024-12-30 20:28:05 -0500

    growingText.jsx update
    capitalizing the file name to match reacts naming scheme, minor bug fixes, and added comments

--- client/src/components/growingText.jsx
+++ client/src/components/growingText.jsx
@@ -2,21 +2,25 @@
 
 import React, { useEffect, useRef, useState } from 'react';
 
-function growingText({text, parentDiv}) {
+function GrowingText({text, parentDiv}) {
   
   const textRef = useRef(null)
+
   const [fontSize, setFontSize] = useState(0.1)
 
   function adjustFontSize() {
     if (parentDiv.current && textRef.current) {
-      let newFontSize = .1
-      textRef.current.style.fontSize = `${newFontSize}rem`
 
+      let newFontSize = .1 // set font size to be tiny
+      textRef.current.style.fontSize = `${newFontSize}rem` // apply new font size to textRef
+
+      // while textRef is smaller than parentDiv
       while ( textRef.current.scrollWidth <= parentDiv.current.clientWidth && textRef.current.scrollHeight <= parentDiv.current.clientHeight ) {
-        newFontSize += .1
-        textRef.current.style.fontSize = `${newFontSize}rem`
+        newFontSize += .1 // increase the font size by a small amount
+        textRef.current.style.fontSize = `${newFontSize}rem` // apply new font size
       }
 
+      // once textRef is larger than parentDiv, decrease the size by a small amount and apply changes
       setFontSize(newFontSize - .2)
     }
   }
@@ -34,10 +38,10 @@ function growingText({text, parentDiv}) {
   }, [fontSize]);
 
   return (
-    <h4 ref={textRef}>
+    <h4 className="growingText" ref={textRef}>
       {text}
     </h4>
     )
 }
 
-export default growingText
+export default GrowingText
