effefe0e304c86c2cce65f9c063b5afb7eb852f9
Mackenzie Neill <128575514+BigBeill@users.noreply.github.com>
2025-02-15 20:39:34 -0500

    Sample Database
    created a separate file in database for holding onto sample users for testing the database

--- server/routes/admin.route.js
+++ server/routes/admin.route.js
@@ -6,46 +6,9 @@ const refreshToken = require("../models/refreshToken");
 const friendship = require("../models/joinTables/friendship");
 const friendRequest = require("../models/joinTables/friendRequest");
 const friendFolder = require("../models/friendFolder");
+const presetUsers = require("../testingSampleData/sampleUsers");
 
-// ------------ preset database data ------------
-const presetUsers = [ 
-   { username: "test", email: "test@gmail.com", bio: "account for testing the database", password: "123"  },
-   { username: "test1", email: "test1@gmail.com", bio: "account for testing the database", password: "123"  },
-   { username: "test2", email: "test2@gmail.com", bio: "account for testing the database", password: "123"  },
-   { username: "test3", email: "test3@gmail.com", bio: "account for testing the database", password: "123"  },
-   { username: "mackenzieNeill", email: "mackenzie@gmail.com", bio: "default account for mackenzie", password: "123"  },
-   { username: "connorPink", email: "connor@gmail.com", bio: "default account for connor", password: "123"  },
-   { username: "alexSmith", email: "alex@gmail.com", bio: "default account for alex", password: "123" },
-   { username: "sarahJohnson", email: "sarah@gmail.com", bio: "default account for sarah", password: "123" },
-   { username: "michaelBrown", email: "michael@gmail.com", bio: "default account for michael", password: "123" },
-   { username: "emilyDavis", email: "emily@gmail.com", bio: "default account for emily", password: "123" },
-   { username: "davidMartinez", email: "david@gmail.com", bio: "default account for david", password: "123" },
-   { username: "jamesGarcia", email: "james@gmail.com", bio: "default account for james", password: "123" },
-   { username: "isabellaMiller", email: "isabella@gmail.com", bio: "default account for isabella", password: "123" },
-   { username: "ethanLopez", email: "ethan@gmail.com", bio: "default account for ethan", password: "123" },
-   { username: "oliviaWilson", email: "olivia@gmail.com", bio: "default account for olivia", password: "123" },
-   { username: "danielAnderson", email: "daniel@gmail.com", bio: "default account for daniel", password: "123" },
-   { username: "avaThomas", email: "ava@gmail.com", bio: "default account for ava", password: "123" },
-   { username: "lucasTaylor", email: "lucas@gmail.com", bio: "default account for lucas", password: "123" },
-   { username: "miaMoore", email: "mia@gmail.com", bio: "default account for mia", password: "123" },
-   { username: "benjaminHernandez", email: "benjamin@gmail.com", bio: "default account for benjamin", password: "123" },
-   { username: "harperClark", email: "harper@gmail.com", bio: "default account for harper", password: "123" },
-   { username: "jacksonLee", email: "jackson@gmail.com", bio: "default account for jackson", password: "123" },
-   { username: "sophiaWalker", email: "sophia@gmail.com", bio: "default account for sophia", password: "123" },
-   { username: "aidenHall", email: "aiden@gmail.com", bio: "default account for aiden", password: "123" },
-   { username: "ellaAllen", email: "ella@gmail.com", bio: "default account for ella", password: "123" },
-   { username: "loganYoung", email: "logan@gmail.com", bio: "default account for logan", password: "123" },
-   { username: "scarlettKing", email: "scarlett@gmail.com", bio: "default account for scarlett", password: "123" },
-   { username: "jacobWright", email: "jacob@gmail.com", bio: "default account for jacob", password: "123" },
-   { username: "graceScott", email: "grace@gmail.com", bio: "default account for grace", password: "123" },
-   { username: "liamGreen", email: "liam@gmail.com", bio: "default account for liam", password: "123" },
-   { username: "zacharyAdams", email: "zachary@gmail.com", bio: "default account for zachary", password: "123" },
-   { username: "chloeBaker", email: "chloe@gmail.com", bio: "default account for chloe", password: "123" },
-   { username: "noahNelson", email: "noah@gmail.com", bio: "default account for noah", password: "123" },
-   { username: "averyCarter", email: "avery@gmail.com", bio: "default account for avery", password: "123" },
-   { username: "elijahMitchell", email: "elijah@gmail.com", bio: "default account for elijah", password: "123" },
-   { username: "victoriaPerez", email: "victoria@gmail.com", bio: "default account for victoria", password: "123" }
-]
+require("dotenv").config();
 
 
 
@@ -83,9 +46,6 @@ router.put("/resetUsers", async (req, res) => {
    return res.status(200).json({ message: "users and refresh tokens reset" });
 
 });
-
-require("dotenv").config();
-
 
 
 
