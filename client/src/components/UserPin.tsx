9c4b8d1061d4f74dfaef6a2cb8b04a52965b34f8
Mackenzie Neill <128575514+BigBeill@users.noreply.github.com>
2025-09-07 22:41:57 -0400

    Image Feature utilized by userPin.tsx
    code has been modified to allow userPin.tsx to display images
    
    images being uploaded via profile are now stored inside imageBuffer while waiting to be uploaded

--- client/src/components/UserPin.tsx
+++ client/src/components/UserPin.tsx
@@ -7,6 +7,8 @@ import axios from '../api/axios';
 import GrowingText from './GrowingText';
 import UserObject from '../interfaces/UserObject';
 
+const database = import.meta.env.VITE_SERVER_LOCATION;
+
 interface UserPinProps {
    userObject: UserObject;
 }
@@ -63,7 +65,10 @@ export default function UserPin({ userObject: parentUserObject }: UserPinProps) 
             <GrowingText text={userObject.username} parentDiv={titleRef} />
          </div>
          <div onClick={ () => { viewProfile() } }>
-            <img src='/profile-photo.png' alt='profile picture' />
+            <img 
+               src={userObject.image?.filename ? `${database}${userObject.image.url}` : "/profile-photo.png"} 
+               alt='profile picture' 
+            />
          </div>
 
          <div className='styleDiv'></div>
